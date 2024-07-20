df144f49c4ecb1c2dfdfaf265c475ac9d1391b18
iameva01 <evatagirova@gmail.com>
2024-07-20 23:04:17 +0300

    Add files via upload

--- module.2.4.py
+++ module.2.4.py
@@ -1,11 +1,13 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []  # пустой список для простых чисел
 not_primes = []  # пустой список для остальных
-for i in numbers:  # перебираем каждое число в numbers, i - каждое число из списка
-    if i < 2:  # проверяем, меньше ли текущее число, чем 2 (не является простым)
+for i in numbers:
+    if i == 1:
+        continue
+    if i < 2:
         not_primes.append(i)
         continue
-    is_prime = True  # (предполагаем, что i - простое, и ставим переменную в True)
+    is_prime = True
     for j in range(2, i):
         if i % j == 0:
             is_prime = False
